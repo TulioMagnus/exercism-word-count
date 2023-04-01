3c0de1017dd492f1993d1d6ad5b98f8125844000
TulioMagnus <tuliokpmagnus@gmail.com>
2023-04-01 17:11:00 -0300

    finished testing

--- word_count.rb
+++ word_count.rb
@@ -1,8 +1,15 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
+  def word_count
+    scan_phrase.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
+  end
 
+  private
+
+  def scan_phrase
+    @phrase.downcase.scan(/\b[\w']+\b/)
+  end
+end
